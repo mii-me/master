08b20bd7767c0d0024b5acd9627e7697d00cda01
PotatoHG <hyeon030111@naver.com>
2021-04-02 09:11:05 +0900

    경매장 찜버튼 제거, 디자인 제외 구현 완료 [by 김현규]

--- 2021_webProject_MiiMe_art/src/main/java/com/art/dao/AuctionDao.java
+++ 2021_webProject_MiiMe_art/src/main/java/com/art/dao/AuctionDao.java
@@ -13,7 +13,6 @@ public class AuctionDao {
 
 	//경매장에서 보여지는 작품 정보
 	public List<AuctionVo> detailAuction(int artNo){
-		System.out.println("dao에서 detailAuction 호출");
 		return DBManager.detailAuction(artNo);
 	}
 	
@@ -34,44 +33,36 @@ public class AuctionDao {
 	
 	//핫딜 조건별 조회1, 찜 횟수(wishList_tb에서 count로 참조)	
 	public List<AuctionVo> listHot_1() {
-		System.out.println("dao에서 listHot_1 호출");
 		return DBManager.listHot_1();
 	}
 	
 	//핫딜 조건별 조회2, 가장 입찰 횟수가 높은 작품
 	public List<AuctionVo> listHot_2() {
-		System.out.println("dao에서 listHot_2 호출");
 		return DBManager.listHot_2();
 	}
 	
 	//핫딜 조건별 조회3, 마감임박 작품
 	public List<AuctionVo> listHot_3() {
-		// TODO Auto-generated method stub
-		System.out.println("dao에서 listHot_3 호출");
 		return DBManager.listHot_3();
 	}
 	
 	//작품보기 임시 구현 (artList.html)	
 	public List<AuctionVo> listArts() {
-		System.out.println("dao에서 listArts 호출");
 		return DBManager.listArts();
 	}
 	
 	//경매장에서 작품에 대한 마감시간 표시
 	public AuctionVo remainTime(int artNo) {
-		System.out.println("AuctionDao에서 remainTime 동작");
 		return DBManager.remainTime(artNo);
 	}
 
 	//찜하기
 	public int insertWish(int artNo,int memNo) {
-		System.out.println("AuctionDao에서 insertWish 동작");
 		return DBManager.insertWish(artNo,memNo);
 	}
 	
 	//찜취소
 	public int deleteWish(int artNo,int memNo) {
-		System.out.println("AuctionDao에서 deleteWish 동작");
 		return DBManager.deleteWish(artNo,memNo);
 	}
 
