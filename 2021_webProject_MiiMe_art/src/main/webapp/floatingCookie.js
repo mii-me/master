6f44c2d95ea0f976f335958e8919aa33e179682d
SeoHyunik <hos0917@naver.com>
2021-04-04 11:07:41 +0900

    by현익 / 플로팅바 오류 수정

--- 2021_webProject_MiiMe_art/src/main/webapp/floatingCookie.js
+++ 2021_webProject_MiiMe_art/src/main/webapp/floatingCookie.js
@@ -30,30 +30,83 @@
 			//alert( cookieArr.length);
 			//alert( cookieArr[0]);
 			//쿠키가 비어있다면 "최근 본 작품 없음" 출력 / 210403
+			
 			if(cookieArr[0]=="" || cookieArr[0]==null){
 				//alert("보여줄 그림 없음");
 				let nullMsg = $("<p></p>").html("최근 본 작품 없음");
 				$("#images").append(nullMsg);
+					
 			}else{
 				//링크도 설정해줘야 한다. /  210402
-				let i = cookieArr.length
-				for(let j= i-1; j>= i-3 ; j--){
-					let artPic = cookieArr[j];
-					let data = {artPic : artPic}
+				let i = cookieArr.length;
+				let artPic;
+				let data;
+				let img;
+				let hr;
+				
+				if(i == 1){
+					artPic = cookieArr[0];
+					console.log(artPic);
+					
+					data = {artPic : artPic}
 					setTimeout(function() {  
 						$.ajax({url:"/findArtElements.do", data:data, success:function(av){
-						let img = $("<a></a>")
+						img = $("<a></a>")
 						.attr("href","artDetail.html?memNo="+av.memNo+"&artNo="
 																		+av.artNo+"&tag="+av.artTag1)
 						.html($("<img>")
-						.attr("src","./art_pic/"+cookieArr[j]).css("width","100px","height","70px"));
+						.attr("src","./art_pic/"+cookieArr[0]).css("width","100px","height","100px"));
 						//images div에 추가해준다.
 						$("#images").append(img);
-						let hr = $("<hr>");
+						hr = $("<hr>");
 						$("#images").append(hr);
 						}});				
-					}, 200);
-				}//end for
+					}, 200);					
+					
+					
+				}else if(i == 2){
+					
+					for(let k=i-1; k>=i-2; k--){
+						artPic =  cookieArr[k];
+						console.log(artPic);
+						
+						data = {artPic : artPic}
+						setTimeout(function() {  
+							$.ajax({url:"/findArtElements.do", data:data, success:function(av){
+							img = $("<a></a>")
+							.attr("href","artDetail.html?memNo="+av.memNo+"&artNo="
+																			+av.artNo+"&tag="+av.artTag1)
+							.html($("<img>")
+							.attr("src","./art_pic/"+cookieArr[k]).css("width","100px","height","100px"));
+							//images div에 추가해준다.
+							$("#images").append(img);
+							hr = $("<hr>");
+							$("#images").append(hr);
+							}});				
+						}, 200);
+					}					
+					
+				}else{
+					for(let j= i-1; j>= i-3 ; j--){
+						artPic = cookieArr[j];
+						console.log(artPic);
+					
+						data = {artPic : artPic}
+						setTimeout(function() {  
+							$.ajax({url:"/findArtElements.do", data:data, success:function(av){
+							img = $("<a></a>")
+							.attr("href","artDetail.html?memNo="+av.memNo+"&artNo="
+																			+av.artNo+"&tag="+av.artTag1)
+							.html($("<img>")
+							.attr("src","./art_pic/"+cookieArr[j]).css("width","100px","height","100px"));
+							//images div에 추가해준다.
+							$("#images").append(img);
+							hr = $("<hr>");
+							$("#images").append(hr);
+							}});				
+						}, 200);
+					}//end for
+				}
 			}//end else
 		}//showCookie
 ////////////////////////////////////////////////////////////////////////////////////////////////		
