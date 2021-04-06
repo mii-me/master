c577a47d1ae36c804adf0de6a712acfd32f71ccf
SHIN-JIYOUNG <74534445+A-Computer-Geek@users.noreply.github.com>
2021-04-06 11:10:45 +0900

    각 메소드 작동에 대한 주석처리, 디버깅 코드 삭제 [by 신지영]

--- 2021_webProject_MiiMe_art/src/main/java/com/art/controller/ReviewController.java
+++ 2021_webProject_MiiMe_art/src/main/java/com/art/controller/ReviewController.java
@@ -27,6 +27,11 @@ public class ReviewController {
 	
 	static int totalRecord;
 
+	/**
+	 * 이용자가 작성한 글을 서버에 등록하기 위한 메소드 
+	 * @param r
+	 * @param request
+	 */
 	@RequestMapping("/insertReviewProto.do")
 	public String insertReviewProto(ReviewVo r, HttpServletRequest request) {
 		String path = request.getRealPath("img");
@@ -34,35 +39,35 @@ public class ReviewController {
 		MultipartFile uploadFile = r.getUploadFile();
 
 		String fname = uploadFile.getOriginalFilename();
-//		System.out.println(fname);
 
 		if (fname != null && !fname.equals("")) {
 			try {
 				byte[] data = uploadFile.getBytes();
 				FileOutputStream fos = new FileOutputStream(path + "/" + fname);
-//				System.out.println(fos);
 				fos.write(data);
 				fos.close();
 			} catch (Exception e) {
 				System.out.println("예외발생:" + e.getMessage());
 			}
-
 		}
 
 		r.setReviewImage(fname);
 		int re = dao.insertReview(r);
 
-		System.out.println(r);
 		return re + "";
 	}
 
+	
+	/**
+	 * 실시간 후기를 받아올 메소드
+	 * @return
+	 */
 	@RequestMapping("/detailRealTimeReviewProto.do")
 	public String detailRealTimeReviewProto() { // 검색, 정렬 기능 없으니까 keyword, searchField, orderField 삭제
 
 		totalRecord = dao.getTotalRecord("", ""); // 모두 값이 없으니까 null로 보낸다
 
-
-		List<ReviewVo> list = dao.findRealTimeAll(); //
+		List<ReviewVo> list = dao.findRealTimeAll();
 
 		/////// java to JSON을 위하여 GSON 사용
 		ReviewResponse r = new ReviewResponse();
@@ -74,10 +79,16 @@ public class ReviewController {
 		return gson.toJson(r);
 	}
 
+	/**
+	 * 실시간 후기 중 좋아요 상위 3개만 뽑아올 메소드 
+	 * @return
+	 */
 	@RequestMapping("/detailRealTimeReviewMost3LikeProto.do")
 	public String detailRealTimeReviewMost3LikeProto() { // 검색, 정렬 기능 없으니까 keyword, searchField, orderField 삭제
+		
 		totalRecord = dao.getTotalRecord("", ""); // 모두 값이 없으니까 null로 보낸다
-		List<ReviewVo> list = dao.findRealTimeMost3LikeAll(); //
+		
+		List<ReviewVo> list = dao.findRealTimeMost3LikeAll();
 
 		/////// java to JSON을 위하여 GSON 사용
 		ReviewResponse r = new ReviewResponse();
@@ -89,15 +100,22 @@ public class ReviewController {
 		return gson.toJson(r);
 	}
 
+	/**
+	 * 사용자가 키워드를 이용하여 검색시 그 키워드에 해당하는 후기를 불러오는 메소드 
+	 * @param keyword: 사용자가 입력한 키워드 
+	 * @param searchField: 그 키워드가 속하는 필드 
+	 * @param orderField: 정렬 기준 
+	 * @return
+	 */
 	@RequestMapping("/detailSearchReviewProto.do")
-	public String detailReviewProto( String keyword, String searchField, String orderField) {
-		System.out.println("keyword:" + keyword);
-		System.out.println("searchField:" + searchField);
-		System.out.println("orderField:" + orderField);
+	public String detailReviewProto(String keyword, String searchField, String orderField) { //패러미터로 키워드, 서치필드, 오더필드 받기 
+//		System.out.println("keyword:" + keyword);
+//		System.out.println("searchField:" + searchField);
+//		System.out.println("orderField:" + orderField);
 
-		totalRecord = dao.getTotalRecord(keyword, searchField); // op는 없으니까 null로 보낸다
+		totalRecord = dao.getTotalRecord(keyword, searchField);
 
-		List<ReviewVo> list = dao.findSearchAll(keyword, searchField, orderField); // op는 없으니까 뺌
+		List<ReviewVo> list = dao.findSearchAll(keyword, searchField, orderField);
 
 		/////// java to JSON을 위하여 GSON 사용
 		ReviewResponse r = new ReviewResponse();
@@ -107,13 +125,19 @@ public class ReviewController {
 		return gson.toJson(r);
 	}
 
+	/**
+	 * 사용자가 테마 키워드를 클릭하여 리뷰에 진입 시 그 키워드에 해당하는 후기를 불러오는 메소드 
+	 * @param keyword: 테마 키워드 
+	 * @param orderField: 정렬 기준(=테마 분류)
+	 * @return
+	 */
 	@RequestMapping("/detailThemeReviewProto.do")
 	public String detailThemeReviewProto(String keyword, String orderField) {
-		System.out.println("keyword:" + keyword);
-		System.out.println("orderField:" + orderField);
+//		System.out.println("keyword:" + keyword);
+//		System.out.println("orderField:" + orderField);
 
-		totalRecord = dao.getTotalRecord(keyword, ""); // op는 없으니까 null로 보낸다
-		List<ReviewVo> list = dao.findThemeAll(keyword, orderField); // op는 없으니까 뺌
+		totalRecord = dao.getTotalRecord(keyword, "");
+		List<ReviewVo> list = dao.findThemeAll(keyword, orderField);
 
 		/////// java to JSON을 위하여 GSON 사용
 		ReviewResponse r = new ReviewResponse();
@@ -123,10 +147,16 @@ public class ReviewController {
 		return gson.toJson(r);
 	}
 
+	/**
+	 * 사용자가 태그 키워드를 클릭하여 리뷰에 진입 시 그 키워드에 해당하는 후기를 불러오는 메소드 
+	 * @param keyword: 태그 키워드 
+	 * @param orderField: 정렬 기준(=태그 분류)
+	 * @return
+	 */
 	@RequestMapping("/detailTagReviewProto.do")
 	public String detailTagReviewProto( String keyword, String orderField) {
-		System.out.println("keyword:" + keyword);
-		System.out.println("orderField:" + orderField);
+//		System.out.println("keyword:" + keyword);
+//		System.out.println("orderField:" + orderField);
 
 		totalRecord = dao.getTotalRecord(keyword, ""); // op는 없으니까 null로 보낸다
 		List<ReviewVo> list = dao.findTagAll(keyword, orderField); // op는 없으니까 뺌
@@ -139,15 +169,25 @@ public class ReviewController {
 		return gson.toJson(r);
 	}
 
+	/**
+	 * 사용자가 좋아요를 누르면 서버에 좋아요 갯수를 1개 늘려주는 메소드 
+	 * @param reviewNo: pk, 후기번호
+	 * @return
+	 */
 	@RequestMapping("/insertLikeReviewProto.do")
-	public int detailTagReviewProto(int reviewNo) {
+	public int detailTagReviewProto(int reviewNo) { //패러미터로 그 후기가 가지는 고유 번호를 받는다 
 		System.out.println("reviewNo:" + reviewNo);
 		int re = dao.insertLikeReview(reviewNo); 
 		return re;
 	}
 	
+	/**
+	 * 사용자가 좋아요를 한 번 더 누르면 서버에 좋아요 갯수를 1개 줄여주는 메소드 
+	 * @param reviewNo: pk, 후기번호
+	 * @return
+	 */
 	@RequestMapping("/deleteLikeReviewProto.do")
-	public int deleteLikeReviewProto(int reviewNo) {
+	public int deleteLikeReviewProto(int reviewNo) { //패러미터로 그 후기가 가지는 고유 번호를 받는다 
 		System.out.println("reviewNo:" + reviewNo);
 		int re = dao.deleteLikeReview(reviewNo); 
 		return re;
